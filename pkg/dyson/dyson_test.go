3b3dca437c9757fe001dcd68aacc6070779077f7
Vincent Boutour <bob@vibioh.fr>
2018-07-17 13:01:36 +0200

    Adding flags tests

---
+++ pkg/dyson/dyson_test.go
@@ -0,0 +1,42 @@
+package dyson
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Flags(t *testing.T) {
+	var cases = []struct {
+		intention string
+		want      string
+		wantType  string
+	}{
+		{
+			`should add string email param to flags`,
+			`email`,
+			`*string`,
+		},
+		{
+			`should add string password param to flags`,
+			`password`,
+			`*string`,
+		},
+		{
+			`should add string country param to flags`,
+			`country`,
+			`*string`,
+		},
+	}
+
+	for _, testCase := range cases {
+		result := Flags(testCase.intention)[testCase.want]
+
+		if result == nil {
+			t.Errorf("%s\nFlags() = %+v, want `%s`", testCase.intention, result, testCase.want)
+		}
+
+		if fmt.Sprintf(`%T`, result) != testCase.wantType {
+			t.Errorf("%s\nFlags() = `%T`, want `%s`", testCase.intention, result, testCase.wantType)
+		}
+	}
+}
