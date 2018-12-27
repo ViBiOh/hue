ec428e28d8b1a741777e966c08ddf547cf45ffb3
Vincent Boutour <bob@vibioh.fr>
2018-12-27 22:37:36 +0100

    Removing uneeded prometheus

--- cmd/iot/iot.go
+++ cmd/iot/iot.go
@@ -18,7 +18,6 @@ import (
 	"github.com/ViBiOh/httputils/pkg/logger"
 	"github.com/ViBiOh/httputils/pkg/opentracing"
 	"github.com/ViBiOh/httputils/pkg/owasp"
-	"github.com/ViBiOh/httputils/pkg/prometheus"
 	"github.com/ViBiOh/httputils/pkg/server"
 	"github.com/ViBiOh/iot/pkg/dyson"
 	"github.com/ViBiOh/iot/pkg/hue"
@@ -42,7 +41,6 @@ func main() {
 
 	serverConfig := httputils.Flags(fs, ``)
 	alcotestConfig := alcotest.Flags(fs, ``)
-	prometheusConfig := prometheus.Flags(fs, `prometheus`)
 	opentracingConfig := opentracing.Flags(fs, `tracing`)
 	owaspConfig := owasp.Flags(fs, ``)
 	corsConfig := cors.Flags(fs, `cors`)
@@ -61,7 +59,6 @@ func main() {
 
 	serverApp := httputils.New(serverConfig)
 	healthcheckApp := healthcheck.New()
-	prometheusApp := prometheus.New(prometheusConfig)
 	opentracingApp := opentracing.New(opentracingConfig)
 	gzipApp := gzip.New()
 	owaspApp := owasp.New(owaspConfig)
@@ -105,5 +102,5 @@ func main() {
 
 	iotApp.HandleWorker()
 
-	serverApp.ListenAndServe(server.ChainMiddlewares(handler, prometheusApp, opentracingApp, gzipApp, owaspApp, corsApp, authApp), nil, healthcheckApp)
+	serverApp.ListenAndServe(server.ChainMiddlewares(handler, opentracingApp, gzipApp, owaspApp, corsApp, authApp), nil, healthcheckApp)
 }
