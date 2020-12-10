2018378be686b2af8ef06db8236322b00fc7a45f
Vincent Boutour <bob@vibioh.fr>
2020-12-10 22:31:20 +0100

    refactor: Using httputils/renderer copy for migration

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ViBiOh/hue
 go 1.15
 
 require (
+	github.com/ViBiOh/httputils v1.8.2
 	github.com/ViBiOh/httputils/v3 v3.33.1
 	github.com/prometheus/client_golang v1.8.0
 	google.golang.org/protobuf v1.25.0 // indirect
---
+++ pkg/hue/templates.go
@@ -0,0 +1,47 @@
+package hue
+
+import "html/template"
+
+var (
+	// FuncMap for template rendering
+	FuncMap = template.FuncMap{
+		"battery": func(value uint) string {
+			switch {
+			case value >= 90:
+				return "battery-full?fill=limegreen"
+			case value >= 75:
+				return "battery-three-quarters?fill=limegreen"
+			case value >= 50:
+				return "battery-half?fill=darkorange"
+			case value >= 25:
+				return "battery-quarter?fill=darkorange"
+			default:
+				return "battery-empty?fill=crimson"
+			}
+		},
+		"temperature": func(value float32) string {
+			switch {
+			case value >= 28:
+				return "thermometer-full?fill=crimson"
+			case value >= 24:
+				return "thermometer-three-quarters?fill=darkorange"
+			case value >= 18:
+				return "thermometer-half?fill=limegreen"
+			case value >= 14:
+				return "thermometer-half?fill=darkorange"
+			case value >= 10:
+				return "thermometer-quarter?fill=darkorange"
+			case value >= 4:
+				return "thermometer-empty?fill=crimson"
+			default:
+				return "snowflake?fill=royalblue"
+			}
+		},
+		"groupName": func(groups map[string]Group, id string) string {
+			if group, ok := groups[id]; ok {
+				return group.Name
+			}
+			return ""
+		},
+	}
+)
