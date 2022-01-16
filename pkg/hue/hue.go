3da36c504c7206b4f0778193913975cab11cc679
Vincent Boutour <bob@vibioh.fr>
2022-01-16 18:30:18 +0100

    chore(deps): Bumping httputils for using flags
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/hue/hue.go
+++ pkg/hue/hue.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/ViBiOh/httputils/v4/pkg/flags"
+	"github.com/ViBiOh/flags"
 	"github.com/ViBiOh/httputils/v4/pkg/renderer"
 	"github.com/prometheus/client_golang/prometheus"
 )
