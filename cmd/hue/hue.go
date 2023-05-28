e832a02142c226c4158f087ca0a52b6ec02ac65c
Vincent Boutour <bob@vibioh.fr>
2023-05-28 20:46:44 +0200

    refactor(flags): Updating usage
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/hue/hue.go
+++ cmd/hue/hue.go
@@ -33,6 +33,7 @@ var content embed.FS
 
 func main() {
 	fs := flag.NewFlagSet("hue", flag.ExitOnError)
+	fs.Usage = flags.Usage(fs)
 
 	appServerConfig := server.Flags(fs, "")
 	promServerConfig := server.Flags(fs, "prometheus", flags.NewOverride("Port", uint(9090)), flags.NewOverride("IdleTimeout", 10*time.Second), flags.NewOverride("ShutdownTimeout", 5*time.Second))
