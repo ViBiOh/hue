086bbaa7e698960c87a9861fde708968962cd855
Vincent Boutour <bob@vibioh.fr>
2024-07-02 14:29:32 +0200

    feat: Using health context instead of background one
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/hue/hue.go
+++ cmd/hue/hue.go
@@ -20,7 +20,7 @@ func main() {
 	go clients.Start()
 	defer clients.Close(ctx)
 
-	services, err := newServices(ctx, config, clients)
+	services, err := newServices(clients.health.EndCtx(), config, clients)
 	logger.FatalfOnErr(ctx, err, "client")
 
 	go services.Start(clients.health.DoneCtx())
