7394a164d7aff887994635411abccfa560995b4f
Vincent Boutour <bob@vibioh.fr>
2025-06-29 10:38:40 +0200

    chore: Metric change back

--- pkg/v2/metrics.go
+++ pkg/v2/metrics.go
@@ -6,7 +6,7 @@ import (
 	"go.opentelemetry.io/otel/metric"
 )
 
-const motionValue = 2
+const motionValue = 1
 
 func (s *Service) createMetrics(meterProvider metric.MeterProvider) error {
 	if meterProvider == nil {
